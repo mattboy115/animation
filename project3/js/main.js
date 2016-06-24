744c601cee18c38b1b71acf76c83cef89abc4067
Matt Lee <matthew_c_lee@yahoo.com>
2016-06-23 21:42:34 -0400

    jkl;jkldsfsd

--- project3/js/main.js
+++ project3/js/main.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
-  $('.carousel').slick({
-    autoplay: true,
-    arrows: true,
-    dots: true,
-    draggable: true,
-    responsive: true,
-    respondTo: window
-  });
+    if (Modernizr.canvas) {
+
+        $('.carousel').slick({
+            autoplay: true,
+            arrows: true,
+            dots: true,
+            draggable: true,
+            responsive: true,
+            respondTo: window
+        });
+
+        $(".dropdown").hover(function() {
+            $(this).children("ul").slideToggle();
+        });
+
+        $(".video_toggle button").click(function() {
+            $(".video_toggle video").slideToggle(150);
+        });
+
+        $(".audio_toggle button").click(function() {
+            $('.audio_toggle audio').toggleClass('hidden', '.notHidden');
+        });
+
+    }
+
+    else {
+        alert("This browser doesn't support canvas.");
+    }
 });
